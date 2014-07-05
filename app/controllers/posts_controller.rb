16323b0e3a87486b84362b76049ea794496d9bd3
Chad <cedie@hotmail.com>
2014-07-05 12:00:04 -0700

    removed edit and delete ability for posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,25 +40,25 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    #respond_to do |format|
+    #  if @post.update(post_params)
+    #    format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+    #    format.json { head :no_content }
+    #  else
+    #    format.html { render action: 'edit' }
+    #    format.json { render json: @post.errors, status: :unprocessable_entity }
+    #  end
+    #end
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+    #@post.destroy
+    #respond_to do |format|
+    #  format.html { redirect_to posts_url }
+    #  format.json { head :no_content }
+    #end
   end
 
   private
